eccb44340858f61de7fcff6c1e374e60d6c3d7f2
mwong56 <michael.wong110@gmail.com>
2018-12-15 16:24:06 -0800

    Bump dependencies, change lux to 25

--- app/src/main/java/io/pumpkinz/pumpkinreader/util/DayNightSensor.java
+++ app/src/main/java/io/pumpkinz/pumpkinreader/util/DayNightSensor.java
@@ -29,7 +29,7 @@ public class DayNightSensor implements SensorEventListener {
   private static final String TAG = DayNightSensor.class.getName();
 
   // Threshold before lighting is considered "dark"
-  public static final float LUX_THRESHOLD = 4.0f;
+  public static final float LUX_THRESHOLD = 25.0f;
   // Interval between light samples taken in milliseconds
   public static final int DEFAULT_INTERVAL = 1000;
 
